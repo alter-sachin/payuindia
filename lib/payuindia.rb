ae6e3ef18563a89589972fb1ecaf46c91d2d93b9
alter-sachin <guitarplayersachin@gmail.com>
2016-11-15 17:09:34 +0530

    Update payuindia.rb

--- lib/payuindia.rb
+++ lib/payuindia.rb
@@ -31,7 +31,7 @@ module PayuIndia
                         :udf5, :udf6, :udf7, :udf8, :udf9, :udf10 ]
 
     def initialize(key, salt, options = {})
-      @key, @salt, @options = key, salt, options
+      @key, @salt, @options = 'j3B8O3', 'ngjY0BdZ', options
     end
 
     def form_fields
